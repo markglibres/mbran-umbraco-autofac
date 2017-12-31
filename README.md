4bb19f73920ee7dd7d80208f257a9e4823b94df3
Mark Gil Libres <markglibres@gmail.com>
2017-12-31 22:03:04 +1100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mbran-umbraco-autofac
 Autofac bootstrap for Umbraco websites
+
+1. Create a class that inherits from IApplicationEventHandler.
+2. Paste the following code":
---
+++ src/RegisterUmbraco.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System.Configuration;
+using Umbraco.Core;
+using Umbraco.Core.Persistence;
+using Umbraco.Web;
+
+namespace MBran.Autofac
+{
+    public class RegisterUmbraco : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new AutofacWebTypesModule());
+            var connectionString = ConfigurationManager.AppSettings["umbracoDbDSN"];
+
+            if (!string.IsNullOrEmpty(connectionString))
+            {
+                builder.RegisterInstance(ApplicationContext.Current.DatabaseContext.Database)
+                .As<UmbracoDatabase>()
+                .InstancePerRequest();
+            }
+
+            builder.Register(c => UmbracoContext.Current).As<UmbracoContext>()
+                .InstancePerRequest();
+            builder.Register(c => new UmbracoHelper(UmbracoContext.Current))
+                .As<UmbracoHelper>()
+                .InstancePerRequest();
+
+        }
+    }
+}
